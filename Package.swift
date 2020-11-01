11968548979c9a30b97f4135a58f6885e95a9614
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2020-11-01 16:02:38 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(name: "GaugeMeterView",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "GaugeMeterView",
+                                          targets: ["GaugeMeterView"])],
+                      targets: [.target(name: "GaugeMeterView",
+                                        path: ".")],
+                      swiftLanguageVersions: [.v5])
+                      
